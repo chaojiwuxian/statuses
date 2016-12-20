0de1c736fb535ad3cf800f6f4aa7b4ad5205e931
超仔 <1065750079@qq.com>
2016-12-20 13:10:51 +0800

    添加授权控制器

---
+++ LiuChaoWeibo/LiuChaoWeibo/Classes/View/OAuth/WBOAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBOAuthViewController.h
+//  LiuChaoWeibo
+//
+//  Created by apple on 16/12/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBOAuthViewController : UIViewController
+
+@end
